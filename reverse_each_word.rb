e85406e0492c914e3830268eb4a45359e7017cef
Jenn Cacace <jenn.cacace@gmail.com>
2018-01-15 12:26:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
-  array = str.split("")
+  array = str.split
   array.each do |word|
     new_array >> word.reverse
-    puts new_array.join
   end 
+  puts new_array.join
 end 
